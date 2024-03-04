255abe69b42745e449618f12502132b6861f4e1c
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2024-03-04 14:48:37 -0600

    Snakes and Ladders

---
+++ Incomplete/snakesAndLadders.java
@@ -0,0 +1,70 @@
+class Solution {
+    int minMoves=Integer.MAX_VALUE;
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        // int n=7;
+        // for (int i = 1; i < n * n; i++) {
+        //     int[] indexes = findRowCol(i, n);
+        //     System.out.println("Number: " + i + " and row, col =[" + indexes[0] + "," + indexes[1] + "]");
+        // }
+        for (int i = 1; i <= 6; i++) {
+            int[] indexes = findRowCol(i,n);
+            snakesAndLadders(board,n,indexes[0],indexes[1],1,i);
+        }
+        return minMoves;
+    }
+
+    public void snakesAndLadders(int[][] board,int n, int row, int col, int moves, int number) {
+        if (row == 0 && col == 0) {
+            System.out.println(moves);
+            minMoves=Math.min(minMoves,moves);
+            return;
+        }
+        if(row<0 || row >=n || col<0 || col>=n ){
+            return;
+        }
+        for (int i = 6; i >= 0; i--) {
+            int[] indexes = findRowCol(number+i,n);
+            int value=board[indexes[0]][indexes[1]];
+            if(value==-1){
+                snakesAndLadders(board,n,indexes[0],indexes[1],moves+1,number+i);
+            }else{
+                indexes = findRowCol(value,n);
+                snakesAndLadders(board,n,indexes[0],indexes[1],moves+1,number+i);
+            }
+           
+        }
+        return;
+    }
+
+    public int[] findRowCol(int number, int n){
+        int row = n - (number/n) - 1; // Bcz number starts from below
+        int col = number % n;
+        if(col==0){
+            row=row+1;
+            if((number/n)%2==1){
+                col=n-1;
+            }
+            else{
+                col=0;
+            }
+            return new int[]{row,col};
+        }
+        if(n%2==0){ // Ex 6 6 matrix
+            if(row%2==1){
+                col=col-1;
+
+            }else{
+                col=n-col;
+            }
+        }else{ // for 7 7 matrix
+            if(row%2==1){
+                col=n-col;
+
+            }else{
+                col=col-1;
+            }
+        }
+        return new int[]{row, col};
+    }
+}
