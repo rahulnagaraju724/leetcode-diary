9388790b86e9c1ec4dd76feb0895dbe25ee235f0
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-06-21 08:49:31 -0500

    Readme and other files updated

--- Arrays/minimumNumberOfMovesToSeatEveryone.java
+++ Arrays/minimumNumberOfMovesToSeatEveryone.java
@@ -14,6 +14,24 @@ class Solution {
 
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
+        int[] pos = new int[101];
+        int n = seats.length;
+        for(int i = 0; i < n; i++){
+            pos[seats[i]]++;
+            pos[students[i]]--;
+        }
+        int res = 0;
+        int current = 0;
+        for(int i: pos){
+            res += Math.abs(current);
+            current += i;
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
         int ans = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         PriorityQueue<Integer> student = new PriorityQueue<>(Collections.reverseOrder());
