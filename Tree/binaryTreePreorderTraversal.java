14cb959a8b5d83fa8565efa75ef201c7daaa07b2
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-08-31 23:36:38 -0500

    PreOrder Iterative Conventional Method

--- Tree/binaryTreePreorderTraversal.java
+++ Tree/binaryTreePreorderTraversal.java
@@ -30,7 +30,7 @@ class Solution {
         preorderTraversal(root.right,preorder);
     }
 
-    //Iterative
+    // Iterative - my method
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> preorder=new ArrayList<>();
         Stack<TreeNode> stack=new Stack<>();
@@ -42,6 +42,26 @@ class Solution {
             }
             root=stack.pop();
             root=root.right;
+        }
+        return preorder;
+    }
+
+    // Iterative - conventional method
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preorder=new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode root = stack.pop();
+            preorder.add(root.val);
+            // Push right and then left child to stack
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
         }
         return preorder;
     }
