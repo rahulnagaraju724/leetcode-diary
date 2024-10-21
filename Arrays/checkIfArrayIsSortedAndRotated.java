7aab053274f431f567340e967889081215709b44
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-10-20 22:59:52 -0500

    added one more solution to previous solution

--- Arrays/checkIfArrayIsSortedAndRotated.java
+++ Arrays/checkIfArrayIsSortedAndRotated.java
@@ -28,7 +28,23 @@ class Solution {
 
         return true;
     }
+    
+//More readable solution
 
+class Solution {
+    public boolean check(int[] arr) {
+        int drop = 0;
+        for(int i = 1; i < arr.length; i++){
+            // Count drops where the current element is greater than the next one
+            if(arr[i - 1] > arr[i]){
+                drop++;
+            }
+        }
 
-
-n
+        // Check if there is a drop from the last element to the first element
+        if(arr[0] < arr[arr.length - 1]) drop++;
+        
+        // The array is sorted or can be sorted with at most one rotation if drop count <= 1
+        return drop <= 1;
+    }
+}
