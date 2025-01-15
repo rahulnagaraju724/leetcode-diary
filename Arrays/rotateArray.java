b6150a79a0542a12c3563893cafc7011dddeb8c4
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-01-15 14:39:59 -0600

    edited rotate array

--- Arrays/rotateArray.java
+++ Arrays/rotateArray.java
@@ -1,7 +1,16 @@
+// 1,2,3,4,5,6,7
+// 5,6,7,1,2,3,4
+
+// First swap at rotate point
+// 4,3,2,1,7,6,5
+// then swap all
+// 5,6,7,1,2,3,4
+
 class Solution {
     public void rotate(int[] nums, int k) {
         //189. Rotate Array
         // First reverese entire array, and then reverse partitions
+        // Time complexity: O(n+n-k+k)=O(2n) and Space Complexity: O(n)
         reverse(nums,0,nums.length-1);
         k=k%nums.length;
         reverse(nums,0,k-1);
@@ -74,4 +83,31 @@ public class Solution {
         return (a == 0 || b == 0) ? a + b : findGcd(b, a % b);
     }
     
+}
+
+
+// Bad solution
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        if(n<2){
+            return;
+        }
+        k=k%n;
+        reverse(nums,0,k);
+        reverse(nums,k,n);
+        reverse(nums,0,n);
+    }
+
+    // Somewhere logic wrong
+    public void reverse(int[] nums, int i, int n){
+        // int middle=(i+n)/2; // INcorrect for n exclusive
+        int middle = i + (n - i) / 2;
+        int j=n-1;
+        for(;i<middle;i++,j--){
+            int temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+        }
+    }
 }
