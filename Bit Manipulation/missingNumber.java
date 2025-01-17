bf8c937b98d23130b55bc36e29497a93f9a78468
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-01-17 07:20:29 -0600

    add time complexity to missing number

--- Bit Manipulation/missingNumber.java
+++ Bit Manipulation/missingNumber.java
@@ -1,3 +1,37 @@
+// Time complexity: O(2n) and Space Complexity: O(n)
+class Solution {
+    public int missingNumber(int[] nums) {
+        boolean[] numbersFound=new boolean[nums.length+1];
+
+        for(int num: nums){
+            numbersFound[num]=true;
+        }
+
+        for(int i=0;i<numbersFound.length;i++){
+            if(numbersFound[i]==false){
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// Time complexity: O(n) and Space Complexity: O(1)
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        int n=nums.length;
+
+        return ((n*(n+1))/2) - sum;
+    }
+}
+
+
+// Time complexity: O(n) and Space Complexity: O(1)
 class Solution{
 public int missingNumber(int[] nums) { //xor
     //268. Missing Number
@@ -10,7 +44,7 @@ public int missingNumber(int[] nums) { //xor
 }
 }
 
-
+// Time complexity: O(n) and Space Complexity: O(1)
 public int missingNumber(int[] nums) { //sum
     int len = nums.length;
     int sum = (0+len)*(len+1)/2;
@@ -19,6 +53,7 @@ public int missingNumber(int[] nums) { //sum
     return sum;
 }
 
+// Time complexity: O(nlogn + logn)) and Space Complexity: O(1)
 public int missingNumber(int[] nums) { //binary search
     Arrays.sort(nums);
     int left = 0, right = nums.length, mid= (left + right)/2;
