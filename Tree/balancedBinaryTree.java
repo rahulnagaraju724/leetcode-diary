0b826e625d6993a0ac5489adcf00be6ef7b3f1b0
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-02-04 16:31:02 -0600

    update balancedBinaryTree problem

--- Tree/balancedBinaryTree.java
+++ Tree/balancedBinaryTree.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+
+// But how can this work at each node or each level? Aren't we just checking root here? Which is wrong right?
 class Solution {
     public boolean isBalanced(TreeNode root) {
         //110. Balanced Binary Tree
@@ -35,23 +37,8 @@ class Solution {
     }
 }
 
-
+// Solution starts here
 // One more method
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Result {
     private int height;
     private boolean isBalanced;
@@ -99,5 +86,29 @@ class Solution {
         Result rightResult=checkBalanced(root.right);
         boolean isBalanced=leftResult.isBalanced() && rightResult.isBalanced() && (Math.abs(leftResult.getHeight()-rightResult.getHeight())<=1);
         return new Result(1+Math.max(leftResult.getHeight() , rightResult.getHeight()) , isBalanced);
+    }
+}
+// End solution here
+
+
+// My solution 
+class Solution {
+    boolean balanced=true;
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return balanced;
+    }
+
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int left=height(root.left);
+        int right=height(root.right);
+        if(Math.abs(left-right)>1){
+            balanced=false;
+        }
+        return 1+Math.max(left,right);
     }
 }
